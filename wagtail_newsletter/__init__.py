3cfa975386ee2216769e1593b3af046e1f0001d8
Alex Morega <alex.morega@torchbox.com>
2024-09-06 15:36:29 +0300

    Bump version number to 0.2.1

--- wagtail_newsletter/__init__.py
+++ wagtail_newsletter/__init__.py
@@ -4,7 +4,7 @@ from django.conf import settings
 default_app_config = "wagtail_newsletter.apps.WagtailNewsletterAppConfig"
 
 
-VERSION = (0, 2, 0)
+VERSION = (0, 2, 1)
 __version__ = ".".join(map(str, VERSION))
 
 
